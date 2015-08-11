af12793df12cb3984edda78014ff6876e8493d54
Slawomir Borowiec <przodownik@tlen.pl>
2015-08-11 15:22:09 +0200

    refresh ligs

---
+++ s.cs
@@ -0,0 +1,53 @@
+Have a non-directory at the top, so creating directory top_dir
+Adding /home/przodownik/blog/cxf_javabased_kata/./LICENSE to top_dir
+Adding /home/przodownik/blog/cxf_javabased_kata/./audit.log to top_dir
+Creating filelist for bin
+Creating filelist for build
+Adding /home/przodownik/blog/cxf_javabased_kata/./build.gradle to top_dir
+Adding /home/przodownik/blog/cxf_javabased_kata/./cxfKata.log to top_dir
+Adding /home/przodownik/blog/cxf_javabased_kata/./cxfKataSQL.log to top_dir
+Adding /home/przodownik/blog/cxf_javabased_kata/./gc.log to top_dir
+Creating filelist for gradle
+Adding /home/przodownik/blog/cxf_javabased_kata/./gradle.properties to top_dir
+Adding /home/przodownik/blog/cxf_javabased_kata/./s.cs to top_dir
+Adding /home/przodownik/blog/cxf_javabased_kata/./troubleshooting.log to top_dir
+Creating filelist for src_main
+Creating filelist for src_test
+Categorizing files.
+Finding a working MD5 command....
+Found a working MD5 command.
+Computing results.
+
+
+SLOC	Directory	SLOC-by-Language (Sorted)
+311     src_main        java=311
+273     build           xml=152,sh=121
+127     bin             xml=127
+38      src_test        java=38
+19      top_dir         cs=19
+0       gradle          (none)
+
+
+Totals grouped by language (dominant language first):
+java:           349 (45.44%)
+xml:            279 (36.33%)
+sh:             121 (15.76%)
+cs:              19 (2.47%)
+
+
+
+
+Total Physical Source Lines of Code (SLOC)                = 768
+Development Effort Estimate, Person-Years (Person-Months) = 0.15 (1.82)
+ (Basic COCOMO model, Person-Months = 2.4 * (KSLOC**1.05))
+Schedule Estimate, Years (Months)                         = 0.26 (3.14)
+ (Basic COCOMO model, Months = 2.5 * (person-months**0.38))
+Estimated Average Number of Developers (Effort/Schedule)  = 0.58
+Total Estimated Cost to Develop                           = $ 20,477
+ (average salary = $56,286/year, overhead = 2.40).
+SLOCCount, Copyright (C) 2001-2004 David A. Wheeler
+SLOCCount is Open Source Software/Free Software, licensed under the GNU GPL.
+SLOCCount comes with ABSOLUTELY NO WARRANTY, and you are welcome to
+redistribute it under certain conditions as specified by the GNU GPL license;
+see the documentation for details.
+Please credit this data as "generated using David A. Wheeler's 'SLOCCount'."
